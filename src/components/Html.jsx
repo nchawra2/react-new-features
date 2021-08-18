f5d47571b51a044520d6418d9c88744866151298
nchawra2 <n1chawra@yahoo.com>
2021-08-18 19:55:25 +0530

    image upload validation

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Links from "./MyLinks";
 import Scroll from "./scrollTop/Scroll";
 
 export default function Html() {
+  let [invalid, setInvalid] = useState("");
+
+  let getFile = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      setInvalid("Please select image[ jpg/jpeg ]");
+      return;
+    }
+
+    console.log(file);
+    if (!file.name.match(/\.(jpg|jpeg)$/)) {
+      setInvalid("Please select valid image[ jpg/jpeg ]");
+      return;
+    } else {
+      setInvalid("");
+    }
+    alert("PHOTO UPLOADED");
+  };
+
   return (
     <React.Fragment>
       <Links />
@@ -23,177 +43,31 @@ export default function Html() {
               </h5>
             </div>
           </div>
+          <div className="col-md-4">
+            <div className=" text-white bg-dark p-1">
+              <h5 className="bg-info text-dark p-2">
+                <strong>Image Upload</strong> With Validations
+              </h5>
+            </div>
+          </div>
         </div>
       </div>
       <div className="container mt-2">
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
-        <p className="text-muted">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
-          nobis esse, optio cum a hic, tempore nisi quidem inventore ducimus qui
-          nostrum rerum atque. Modi, reiciendis omnis consequuntur veritatis
-          eligendi nemo optio necessitatibus ducimus nam possimus perferendis
-          dolorum. Fuga, qui.
-        </p>
+        <div className="row mt-5">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-header bg-secondary text-white">
+                <h4>Upload Image With Validations</h4>
+              </div>
+              <div className="card-body">
+                <form>
+                  <input type="file" name="fileInput" onChange={getFile} />
+                </form>
+                <h6 className="text-danger text-uppercase">{invalid}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
